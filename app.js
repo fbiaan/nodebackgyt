3e4a79880e78f7c5a2f8def64716d7051ae02f06
Rimolina <jcechetto@abs-ti.com>
2022-05-27 21:09:10 -0300

    segundo deploy

--- app.js
+++ app.js
@@ -114,6 +114,84 @@ app.get('/prueba', async function(req, res)  {
     }
   });
 
+  app.get('/pagosxmes', async function(req, res)  {
+    try {
+
+        //const sqlQuery = 'Select c.idAlumno, c.idCuota from  Tyg.Cuotas c'
+        
+        const sqlQuery = 'SELECT e.descripcion , h.Sdescripcion ,  \
+            SUM(CASE WHEN pc.desagrupa=\'ENERO\' THEN c.Nimporte ELSE 0 END) AS ENERO, \
+            SUM(CASE WHEN pc.desagrupa=\'FEBRERO\' THEN c.Nimporte ELSE 0 END) AS FEBRERO, \
+            SUM(CASE WHEN pc.desagrupa=\'MARZO\' THEN c.Nimporte ELSE 0 END) AS MARZO, \
+            SUM(CASE WHEN pc.desagrupa=\'ABRIL\' THEN c.Nimporte ELSE 0 END) AS ABRIL, \
+            SUM(CASE WHEN pc.desagrupa=\'MAYO\' THEN c.Nimporte ELSE 0 END) AS MAYO, \
+            SUM(CASE WHEN pc.desagrupa=\'JUNIO\' THEN c.Nimporte ELSE 0 END) AS JUNIO, \
+            SUM(CASE WHEN pc.desagrupa=\'JULIO\' THEN c.Nimporte ELSE 0 END) AS JULIO, \
+            SUM(CASE WHEN pc.desagrupa=\'AGOSTO\' THEN c.Nimporte ELSE 0 END) AS AGOSTO, \
+            SUM(CASE WHEN pc.desagrupa=\'SEPTIEMBRE\' THEN c.Nimporte ELSE 0 END) AS SEPTIEMBRE, \
+            SUM(CASE WHEN pc.desagrupa=\'OCTUBRE\' THEN c.Nimporte ELSE 0 END) AS OCTUBRE, \
+            SUM(CASE WHEN pc.desagrupa=\'NOVIEMBRE\' THEN c.Nimporte ELSE 0 END) AS NOVIEMBRE, \
+            SUM(CASE WHEN pc.desagrupa=\'DICIEMBRE\' THEN c.Nimporte ELSE 0 END) AS DICIEMBRE \
+            FROM Tyg.Cuotas c  \
+            INNER JOIN Tyg.PeriodoCuotas pc ON pc.idPerCuotas = c.idPerCuota  \
+            INNER JOIN Tyg.Planilla p ON p.idPlanilla = c.idPlanilla  \
+            INNER JOIN Tyg.Horarios h ON p.idHorario = h.NidHorario \
+            INNER JOIN Tyg.Edad e ON p.idEdad = e.idEdad \
+            GROUP BY e.descripcion , h.Sdescripcion';
+            
+        const rows = await pool.query(sqlQuery);
+        res.status(200).json(rows);
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+  });
+
+  app.get('/pagosxclase', async function(req, res)  {
+    try { 
+        const sqlQuery = 'SELECT e.descripcion , h.Sdescripcion , \
+                            SUM(c.Nimporte) AS TOTAL \
+                            FROM Cuotas c \
+                            INNER JOIN PeriodoCuotas pc ON pc.idPerCuotas = c.idPerCuota \
+                            INNER JOIN Planilla p ON p.idPlanilla = c.idPlanilla \
+                            INNER JOIN Horarios h ON p.idHorario = h.NidHorario \
+                            INNER JOIN Edad e ON p.idEdad = e.idEdad \
+                            GROUP BY e.descripcion , h.Sdescripcion';
+            
+        const rows = await pool.query(sqlQuery);
+        res.status(200).json(rows);
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+  });
+
+  app.get('/pagostotalxmes', async function(req, res)  {
+    try { 
+        const sqlQuery = 'SELECT \
+        SUM(CASE WHEN pc.desagrupa =\'ENERO\' THEN c.Nimporte ELSE 0 END) AS ENERO, \
+        SUM(CASE WHEN pc.desagrupa =\'FEBRERO\' THEN c.Nimporte ELSE 0 END) AS FEBRERO, \
+        SUM(CASE WHEN pc.desagrupa =\'MARZO\' THEN c.Nimporte ELSE 0 END) AS MARZO, \
+        SUM(CASE WHEN pc.desagrupa =\'ABRIL\' THEN c.Nimporte ELSE 0 END) AS ABRIL, \
+        SUM(CASE WHEN pc.desagrupa =\'MAYO\' THEN c.Nimporte ELSE 0 END) AS MAYO, \
+        SUM(CASE WHEN pc.desagrupa =\'JUNIO\' THEN c.Nimporte ELSE 0 END) AS JUNIO, \
+        SUM(CASE WHEN pc.desagrupa =\'JULIO\' THEN c.Nimporte ELSE 0 END) AS JULIO, \
+        SUM(CASE WHEN pc.desagrupa =\'AGOSTO\' THEN c.Nimporte ELSE 0 END) AS AGOSTO, \
+        SUM(CASE WHEN pc.desagrupa =\'SEPTIEMBRE\' THEN c.Nimporte ELSE 0 END) AS SEPTIEMBRE, \
+        SUM(CASE WHEN pc.desagrupa =\'OCTUBRE\' THEN c.Nimporte ELSE 0 END) AS OCTUBRE, \
+        SUM(CASE WHEN pc.desagrupa =\'NOVIEMBRE\' THEN c.Nimporte ELSE 0 END) AS NOVIEMBRE, \
+        SUM(CASE WHEN pc.desagrupa =\'DICIEMBRE\' THEN c.Nimporte ELSE 0 END) AS DICIEMBRE \
+        FROM Cuotas c \
+        INNER JOIN PeriodoCuotas pc ON pc.idPerCuotas = c.idPerCuota \
+        INNER JOIN Planilla p ON p.idPlanilla = c.idPlanilla \
+        INNER JOIN Horarios h ON p.idHorario = h.NidHorario \
+        INNER JOIN Edad e ON p.idEdad = e.idEdad';
+            
+        const rows = await pool.query(sqlQuery);
+        res.status(200).json(rows);
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+  });
+
   app.get('/planillas', async function(req, res)  {
     try {
         const sqlQuery = 'SELECT idPlanilla , e2.descripcion , h.Sdescripcion , h.Sshortdias, \
